a95bcb1a7704d955a54b66c21cf6fbcd53a4c153
Vedarth Kulkarni <vedarth@recruiterbox.com>
2021-10-19 00:12:31 +0530

    fix imports for Django 2.0 upgrade

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -48,7 +48,7 @@ from tastypie.event_handler import EventHandler
 from tastypie.bundle_pre_processor import BundlePreProcessor
 from tastypie import response_router_obj
 from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import RegexURLResolver
+from django.urls.resolvers import URLResolver as RegexURLResolver
 from django.core.signals import got_request_exception
 
 from copy import copy
