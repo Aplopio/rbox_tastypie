703f3996fcc997d83d984391738ac55fafc45378
Nisarg Raval <nisargraval95@gmail.com>
2021-10-20 15:59:20 +0530

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-tastypie',
-    version='0.14.0',
+    version='0.16.2',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
