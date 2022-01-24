5bc75c0686be1dace1592e52c30b327c94c76c6d
Vedarth Kulkarni <vedarthk@hey.com>
2022-01-24 16:54:42 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-tastypie',
-    version='0.16.9',
+    version='0.17.0',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
